f6f8ab7b28e1ea2241376d21ce7759469e999212
Oleksandr Malomed <oleksandr.malomed@outlook.com>
2023-02-22 12:25:04 +0200

    add mvc project

---
+++ WebAppMvc/Data/WebAppMvcContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using WebAppMvc.Models;
+
+namespace WebAppMvc.Data {
+    public class WebAppMvcContext : DbContext
+    {
+        public WebAppMvcContext (DbContextOptions<WebAppMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Product> Products => Set<Product>();
+
+        public DbSet<WebAppMvc.Models.Category> Category { get; set; } = default!;
+    }
+}
